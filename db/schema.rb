a370bc564be42cc168d9db6af815f44181a99e19
stevatech <stevan.cubrilo96@gmail.com>
2022-08-02 12:28:55 +0200

    solving problem with db:rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_31_202409) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_070132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.string "percent_utilized"
+    t.string "integer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
